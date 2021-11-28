4b2b7a144ed9de5359eb616a609ed2b3e8c5be7d
Selena Shaw <selenas@princeton.edu>
2021-11-28 18:24:14 -0500

    port

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const app = require('./App');
 
 
 const uri = process.env.MONGODB_URI;
+let port = process.env.PORT || 8000;
 
 mongoose.connect(uri).then(() => {
   console.log("Connected to database");
-  app.listen(8000, ()=>{
+  app.listen(port, ()=>{
     console.log("App listening on port 8000");
   });
 });
